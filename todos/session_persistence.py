768b3e46a151624e32da8d7ac5e35dcf97b125b7
Christine Lin <lin.christine19@gmail.com>
2025-10-05 16:32:31 -0400

    move todo functionality to session persistence

--- todos/session_persistence.py
+++ todos/session_persistence.py
@@ -32,17 +32,39 @@ class SessionPersistence:
         self.session['lists'] = [lst for lst in self.session['lists']
                                  if lst['id'] != list_id]
         self.session.modified = True
-        
+
+    def delete_todo_by_id(self, list_id, todo_id):
+        lst = self.find_list(list_id)
+        lst['todos'] = [todo for todo in lst['todos'] if todo['id'] != todo_id]
+
+        self.session.modified = True
 
     def find_list(self, list_id):
         found = (lst for lst in self.session['lists'] if lst['id'] == list_id)
         return next(found, None)
     
+    def mark_all_todos_completed(self, list_id):
+        lst = self.find_list(list_id)
+        for todo in lst['todos']:
+            todo['completed'] = True
+        self.session.modified = True
+
     def update_list_by_id(self, list_id, new_title):
         lst = self.find_list(list_id)
         if lst: 
             lst['title'] = new_title
             self.session.modified = True
+
+    def find_todo(self, list_id, todo_id):
+        lst = self.find_list(list_id)
+        found = (todo for todo in lst['todos'] if todo['id'] == todo_id)
+        return next(found, None)
+
+    def update_todo_status(self, list_id, todo_id, new_status):
+        todo = self.find_todo(list_id, todo_id)
+        todo['completed'] = new_status
+
+        self.session.modified = True
 
 
 
